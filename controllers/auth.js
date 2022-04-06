1eb4098fe22aef25c6ac8866ec99b630764d5cc1
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-06 12:35:57 +0100

    sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
 const User=require("../models/user")
+const {validationResult}=require("express-validator")
 
 exports.signup=(req,res,next)=>{
+    const errors=validationResult(req)
+     if(!errors.isEmpty()){
+
+     }
     const email=req.body.email
     const name=req.body.name
     const password=req.body.password
 
-    
+
 }
