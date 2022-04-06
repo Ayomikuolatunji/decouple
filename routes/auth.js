0cfd6fd1016601a9cd1252348b086f8e9e53dce8
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-06 12:30:26 +0100

    user validation on route api

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,17 @@ const User=require("../models/user")
 
 
 
-router.post("/signup",[body("email").trim().isEmail().withMessage("please enter a valid emil").custom((value,{req})=>{
+router.post("/signup",[
+    body("email").trim().isEmail().withMessage("please enter a valid emil") .custom((value,{req})=>{
       User.findOne({email:value})
       .then(userDoc=>{
-
+          if(userDoc){
+              return Promise.reject("Email address already exits")
+          }
       })
-})])
+        }).normalizeEmail().isLength({min:4}), 
+    body("password").trim().isLength({min:5}),
+    body("name").trim().isLength({min:5})
+])
 
 module.exports=router
