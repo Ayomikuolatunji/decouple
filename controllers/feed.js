611447c59aacde1d73c72c079637af134a3a63f2
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-06 23:45:44 +0100

    async conversion

--- controllers/feed.js
+++ controllers/feed.js
@@ -80,45 +80,34 @@ exports.createPost = (req, res, next) => {
     });
 };
 
-exports.getPost = (req, res, next) => {
-  const postId = req.params.postId;
-  Post.findById(postId)
-    .then(post => {
+exports.getPost = async(req, res, next) => {
+     try{
+      const postId = req.params.postId;
+      const post=await  Post.findById(postId)
       if (!post) {
         const error = new Error('Could not find post.');
         error.statusCode = 404;
         throw error;
       }
       res.status(200).json({ message: 'Post fetched.', post: post });
-    })
-    .catch(err => {
+     }catch(error){
       if (!err.statusCode) {
         err.statusCode = 500;
       }
-      next(err);
-    });
+      next(err); 
+     }
 };
 
 exports.updatePost = (req, res, next) => {
-  const postId = req.params.postId;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect.');
-    error.statusCode = 422;
-    throw error;
-  }
-  const title = req.body.title;
-  const content = req.body.content;
-  let imageUrl = req.body.image;
-  if (req.file) {
-    imageUrl = req.file.path;
-  }
-  if (!imageUrl) {
-    const error = new Error('No file picked.');
-    error.statusCode = 422;
-    throw error;
-  }
-  Post.findById(postId)
+  try{
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect.');
+      error.statusCode = 422;
+      throw error;
+    }
+    Post.findById(postId)
     .then(post => {
       if (!post) {
         const error = new Error('Could not find post.');
@@ -141,12 +130,13 @@ exports.updatePost = (req, res, next) => {
     .then(result => {
       res.status(200).json({ message: 'Post updated!', post: result });
     })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  }catch(error){
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+
 };
 
 exports.deletePost = (req, res, next) => {
