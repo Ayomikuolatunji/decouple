2eccb0ab78bfe60a8ad16d1b2d4e4163e3f806ee
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-06 12:44:51 +0100

    bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,19 @@
 const User=require("../models/user")
 const {validationResult}=require("express-validator")
+const bcrypt=require("bcryptjs")
 
 exports.signup=(req,res,next)=>{
     const errors=validationResult(req)
      if(!errors.isEmpty()){
-
+        const error=new Error("Validation failed")
+        error.statusCode=422
+        error.data=errors.array()
+        throw error
      }
     const email=req.body.email
     const name=req.body.name
     const password=req.body.password
-
-
+    bcrypt.hash(password,15)
+    .then()
+    .catch()
 }
