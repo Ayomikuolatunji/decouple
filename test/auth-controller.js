56ac05a80e26880b62bec2645f84f672dfc37d1a
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-10 07:11:30 +0100

    login auth testing

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,10 +5,20 @@ const authController=require("../controllers/auth")
 const User=require("../models/user")
 
 describe("testing controller-login",()=>{
-    it('should throw an error  with code 500 if accessing the database failed',()=>{
+    it('should throw an error with code 500 if accessing the database failed',()=>{
         Sinon.stub(User,"findOne")
         User.findOne.throws
-
+        const req={
+            body:{
+               email:"Ayomiku@gmail.com",
+               password:"johnjhhh"    
+            }
+        }
+        authController.login(req,{}, ()=>{})
+        .then(result=>{
+            expect(result).to.be.an("error")
+            expect(result).to.have.property("statusCode",500)
+        })
         User.findOne.restore()
     })
 })
