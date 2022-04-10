84ec70e38d86cec23d7e7c91d29b7888857f615b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-10 06:19:39 +0100

    testing auth middleware

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,11 +1,25 @@
 const expect=require("chai").expect
 const authMiddleWare=require("../middleware/is-auth")
 
-it("It should throw an error if no authorization header is present",()=>{
-   const req={
-       get:()=>{
-           return null
-       }
-   }  
-   authMiddleWare(req,{},()=>{})
+
+describe("auth middleware",function(){
+    it("It should throw an error if no authorization header is present",function(){
+        const req={
+            get:()=>{
+                return null
+            }
+        }  
+     
+        expect(authMiddleWare.bind(this,req,{},()=>{})).to.throw("Not authenticated")
+     })
+     
+     it("it should throw error when authorization has only one string",function(){
+         const req={
+             get:()=>{
+                 return "xyz"
+             }
+         }
+         expect(authMiddleWare.bind(this,req,{},()=>{})).to.throw()
+     })
 })
+
